3c94fe46dcfc1344b5919f43762bb15e843a17ef
Anna Krasilnikova <akr6363@mail.ru>
2023-03-01 15:22:05 +0300

    objects

---
+++ src/02/02.ts
@@ -0,0 +1,57 @@
+type LocalCityType = {
+    title: string
+    country: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechnologiesType = {
+    id:number
+    title: string
+}
+
+type StudentsType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechnologiesType>
+}
+
+const student: StudentsType = {
+    id: 1,
+    name: "Dimych",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            country: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id:1,
+            title: 'HTML'
+        },
+        {
+            id:2,
+            title: 'CSS'
+        },
+        {
+            id:3,
+            title: 'JS'
+        },
+    ]
+
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[2].title)
